e82adb11d97c0a9b72c99d8cf055e6a5017f87a5
Alex <aarychagov@gmail.com>
2018-01-19 11:46:23 +0400

    upd

--- app/actions.py
+++ app/actions.py
@@ -3,7 +3,9 @@ import os, re, csv, zipfile
 prezzi = { "75" : 10, "102" : 10, "108" : 10, "109" : 10, "111" : 10, "112" : 10, "113" : 10, "128" : 10, "129" : 10, "141" : 10, "144" :10, "149" : 10, "156" : 10, "162" : 10, "182" : 10, "202" : 10, "206" : 10, "207" : 10, "208" : 10, "211" : 10, "212" : 10, "222" : 10, "226" : 10, "227" : 10, "228" : 10, "229" : 10, "230" : 10, "231" : 10, "232" : 10, "233" : 10, "234" : 10, "237" : 10, "238" : 10, "239" : 10, "240" : 10 }
 # what is prezzi?
 
-def process_folder_to_csv(csv_file, folder):
+csv_file = os.path.join(os.getcwd(), 'csv.csv')
+
+def process_folder_to_csv(folder, csv_file=csv_file):
     csvfile = csv.writer(open(csv_file, 'wb'), delimiter='$', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     csvfile.writerow(['Anno','Mese','Prod_ID','Prodotto','Qta','Reso','Abb','Copertina','Imponibile','IVA dovuta'])
 
@@ -11,7 +13,7 @@ def process_folder_to_csv(csv_file, folder):
         # print root
         for file in files:
             if file.endswith(".st"):
-                print file
+                # print file
                 with open(os.path.join(root, file)) as fp:
                     da_a = re.findall('Dal\s(\d+/\d+/\d+)\sal\s(\d+/\d+/\d+)',fp.read())
                     # print da_a
--- app/main.py
+++ app/main.py
@@ -1,10 +1,54 @@
-from flask import Flask
+from flask import Flask, request, flash, redirect, url_for, make_response
+from werkzeug.utils import secure_filename
+import os
+import actions
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World from Flask in a uWSGI Nginx Docker container with \
-     Python 2.7 (default)"
+UPLOAD_FOLDER = os.getcwd()
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+ALLOWED_EXTENSIONS = {'zip'}
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def return_csv(csv_filename):
+    with open(csv_filename, 'r') as f:
+        response = make_response(f.read())
+        cd = 'attachment; filename=data.csv'
+        response.headers['Content-Disposition'] = cd
+        response.mimetype = 'text/csv'
+        return response
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(url_for('index'))
+        if file and allowed_file(file.filename):
+            zip_file_name = 'data.zip'
+            zip_full_filename = os.path.join(app.config['UPLOAD_FOLDER'], zip_file_name)
+            file.save(zip_full_filename)
+            actions.unzip_file(zip_full_filename)
+            actions.process_folder_to_csv(actions.unzip_folder)
+            return return_csv(actions.csv_file)
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True, port=80)
+    app.run(host='127.0.0.1', debug=True, port=8080)
